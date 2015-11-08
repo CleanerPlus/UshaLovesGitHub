38d644afc98232da58b12c5580be296c679a069a
CleanerPlus <adam@cleaner.plus>
2015-11-08 17:11:20 +0000

    Changes made

--- TestApplication/Form1.cs
+++ TestApplication/Form1.cs
@@ -16,5 +16,10 @@ namespace TestApplication
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
